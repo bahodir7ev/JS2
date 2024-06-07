711bd705374bcddc461697f73c3ea143be4bb966
Админ <macbahodir@gmail.com>
2024-06-07 13:02:50 +0500

    My homework

---
+++ app.js
@@ -0,0 +1,101 @@
+//Arrays
+
+//Array 18
+
+// let n = Number(prompt("Sonni kiriting: "))
+// let arr = [];
+
+// const lastElement = arr[arr.length - 1];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < lastElement) {
+//         console.log(arr[i]);
+//     }
+// }
+
+//Array 19
+
+// let lastIndex = arr.length - 1;
+// let lastItem = lastIndex;
+// let fristItem = arr[0]
+// let javob = null;
+// let javobIndex = null
+
+// console.log(lastIndex);
+// for (i = 1; i <= n.length; i++) {
+//   if (arr[i] < lastItem && arr[i] > fristItem ) {
+//     javob = arr[i]
+//     javobIndex = i
+//   } 
+// }
+
+// if(){
+
+// }
+
+//Array 20
+
+// let n = Number(prompt("Son kiriting: "));
+// let arr = [];
+
+// for (i = 1; i <= n; i++) {
+//     let son = Number(prompt(`${i} sonni kiriting:`))
+//     arr.push(son);
+// }
+// let k = Number(prompt('Birinchi indexni kiritig: '))
+// let l = Number(prompt('Ikkinchi indexni kiriting: '))
+// let yigindi = 0
+// let count = 0
+
+//Array 21
+
+// let array = []
+// let K = Number(prompt("K ni kiriting"));
+// let L = Number(prompt("L ni kiriting"))
+// if (K > L || K < 0 || L >= array.length) K, L ==  null;
+
+// let maxElement = array[K];
+// let maxIndex = K;
+
+// for (let i = K + 1; i <= L; i++) {
+//     if (array[i] > maxElement) {
+//         maxElement = array[i];
+//         maxIndex = i;
+//     }
+// }
+
+// console.log(maxIndex);
+
+//Array 28
+
+// let array = []
+// let minIndex = -1;
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 === 0 && (minIndex === -1 || array[i] < array[minIndex])) {
+//         minIndex = i;
+//     }
+// }
+
+// console.log(minIndex);
+
+// console.log(array);
+
+
+//Array 29
+
+
+// let array = [5, 3, 8, 7, 6];
+// let maxIndex = -1;
+
+// for (let i = 1; i < array.length; i += 2) {
+//     if (maxIndex === -1 || array[i] > array[maxIndex]) {
+//         maxIndex = i;
+//     }
+// }
+
+// console.log(maxIndex);
+// console.log(array);
+
+//Array 
+
